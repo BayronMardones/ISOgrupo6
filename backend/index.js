82d7224e95212ed28bc35753399fb9e3d2bbc349
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-09-01 22:15:28 -0400

    inicio servidor

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => console.log('Server started'));
+
 return console.log('HOLA JUIJUI')
