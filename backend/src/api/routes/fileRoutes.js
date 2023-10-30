32038acf82c028b408d418bda90b03ab5f2e4ace
BayronMardones <bayron.mardones1901@alumnos.ubiobio.cl>
2023-10-30 11:47:02 -0300

    si

--- backend/src/api/routes/fileRoutes.js
+++ backend/src/api/routes/fileRoutes.js
@@ -8,7 +8,6 @@ const routers = Router();
 routers.post("/file/:id/:archivo", checkUserRole(["encargado", "oficinista", "solicitante"]),upload.array('archivos'), fileSize, fileController.uploadfile);
 routers.get('/files', checkUserRole(["admin", "oficinista","encargado"]),fileController.getFiles);
 routers.get('/file/download/:id', checkUserRole(["admin", "oficinista", "encargado", "solicitante"]),fileController.getSpecificFile);
-routers.delete('/file/delete/:idacta',checkUserRole(["admin", "oficinista"]), fileController.deleteFiles);
-routers.delete('/file/deletes/:id', checkUserRole(["admin", "oficinista"]),fileController.deleteFilesS);
+routers.delete('/file/deletes/:id', checkUserRole(["admin", "oficinista"]),fileController.deleteFile);
 
 export default routers;
