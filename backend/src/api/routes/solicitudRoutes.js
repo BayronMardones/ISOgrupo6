3fc22dfd5659ab5aa7064647b346ab8ba4415a2b
rodripgg <rodrigo.parra1901@alumnos.ubiobio.cl>
2023-10-30 09:34:35 -0300

    solo para probar quite los permisos de solicitud

--- backend/src/api/routes/solicitudRoutes.js
+++ backend/src/api/routes/solicitudRoutes.js
@@ -6,7 +6,7 @@ import solicitudController from "../controllers/solicitudController.js";
 const router = Router();
 
 router.get("/listar", checkUserRole(["admin", "encargado", "oficinista"]), solicitudController.listarSolicitudes);
-router.post("/crear", checkUserRole(["oficinista", "solicitante"]), solicitudController.crearSolicitud);
+router.post("/crear", solicitudController.crearSolicitud);
 router.get("/buscar/:id", checkUserRole(["admin", "encargado", "oficinista", "solicitante"]), solicitudController.buscarSolicitudPorId);
 router.put("/actualizar/:id", checkUserRole(["admin", "oficinista", "solicitante"]), solicitudController.actualizarSolicitudPorId);
 router.delete("/eliminar/:id", checkUserRole(["admin"]), solicitudController.eliminarSolicitudPorId);
